be3f8360fbd180d00f0a1844d4781f1c5203b407
Jaron <jaronchretien@yahoo.com>
2024-04-29 20:10:09 -0500

    feat(form): Adds form for quote information

--- src/Components/VouchComponent.js
+++ src/Components/VouchComponent.js
@@ -20,6 +20,54 @@ const Vouch = () => {
             <h1>Chretien Lawn Care Service</h1>
         </div>
         <div className="caraBox two">
+            <div className="inputDivBox one">
+                <div className="nameBox">
+                    <div className="labelInputBox">
+                        <label>First Name</label>
+                        <input></input>
+                    </div>
+                    <div className="labelInputBox">
+                        <label>Last Name</label>
+                        <input></input>
+                    </div>
+                </div>
+            </div>
+            <div className="inputDivBox two">
+                <div className="labelInputBox">
+                    <label>Phone Number</label>
+                    <input/>
+                </div>
+            </div>
+            <div className="inputDivBox two">
+                <div className="labelInputBox">
+                    <label>Email</label>
+                    <input/>
+                </div>
+            </div>
+            <div className="inputDivBox three">
+                <div className="labelInputBox">
+                    <label>Address Line 1 (required)</label>
+                    <input/>
+                </div>
+            </div>
+            <div className="inputDivBox four">
+                <div className="labelInputBox">
+                    <label>Address Line 2</label>
+                    <input/>
+                </div>
+            </div>
+            <div className="inputDivBox five">
+                <div className="nameBox">
+                    <div className="labelInputBox">
+                        <label>City (required) </label>
+                        <input></input>
+                    </div>
+                    <div className="labelInputBox">
+                        <label>ZIP Code (required)</label>
+                        <input></input>
+                    </div>
+                </div>
+            </div>
             <button>Get A Free Quote</button>
         </div>
         
