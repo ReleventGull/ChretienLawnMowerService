fa55a5e1d2462a04a0cac785b01865d48f11d023
Jaron <jaronchretien@yahoo.com>
2024-05-07 12:03:15 -0500

    feat(logo): Adds the official logo to the website

---
+++ src/Components/Map.js
@@ -0,0 +1,11 @@
+
+
+const Map = () => {
+    return (
+        <div className="flex justify-center items-center w-100 bg-red-500">
+            <h1 className="text-3xl">Where do we service?</h1>
+        </div>
+    )
+}
+
+export default Map
