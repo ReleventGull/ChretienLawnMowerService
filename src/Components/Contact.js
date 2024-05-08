ad8ceff06fb0cb04ac1594daefa9e27bedb7996d
Jaron <jaronchretien@yahoo.com>
2024-05-08 13:05:48 -0500

    feat(formFix): Gives form opacity when it is not focused on

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,54 +1,59 @@
 const Contact = () => {
+    const commitFocus = () => {
+        document.getElementsByClassName('contactBox')[0].style.opacity = 1
+    }
+    const commitBlur = () => {
+        document.getElementsByClassName('contactBox')[0].style.opacity = .6
+    }
     return (
         <div>
-
         <div className="contactBox">
         <div className="caraBox">
             <div className="inputDivBox one">
                 <div className="nameBox">
                     <div className="labelInputBox">
                         <label>First Name</label>
-                        <input></input>
+                        <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}></input>
                     </div>
                     <div className="labelInputBox">
                         <label>Last Name</label>
-                        <input></input>
+                        <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}></input>
                     </div>
                 </div>
             </div>
             <div className="inputDivBox two">
                 <div className="labelInputBox">
                     <label>Phone Number</label>
-                    <input/>
+                    <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}/>
                 </div>
             </div>
             <div className="inputDivBox two">
                 <div className="labelInputBox">
                     <label>Email</label>
-                    <input/>
+                    <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}/>
                 </div>
             </div>
             <div className="inputDivBox three">
                 <div className="labelInputBox">
                     <label>Address Line 1 (required)</label>
-                    <input/>
+                    <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}/>
                 </div>
             </div>
             <div className="inputDivBox four">
                 <div className="labelInputBox">
                     <label>Address Line 2</label>
-                    <input/>
+                    <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}/>
                 </div>
             </div>
             <div className="inputDivBox five">
                 <div className="nameBox">
                     <div className="labelInputBox">
                         <label>City (required) </label>
-                        <input></input>
+                        <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}></input>
                     </div>
                     <div className="labelInputBox">
                         <label>ZIP Code (required)</label>
-                        <input></input>
+                        <input onBlur={() => commitBlur()} onFocus={() => commitFocus()}></input>
                     </div>
                 </div>
             </div>
