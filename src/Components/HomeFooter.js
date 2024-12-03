2a99b7f2fb1d95250d5783de2bc9e1a481de8c49
Jaron <jaronchretien@yahoo.com>
2024-12-03 11:31:58 -0600

    feat(footer): Redesigns the footer

--- src/Components/HomeFooter.js
+++ src/Components/HomeFooter.js
@@ -1,23 +1,19 @@
 import { Link } from "react-router-dom"
 const HomeFooter = () => {
     return (
-        <footer className="homeFooter">
-            <div className="socialFooter">
-                <h4>Follow Us</h4>
-                    <div className="socialImageBox">
-                        <img src='images/TikTok.png'/>
-                        <img src='images/Facebook.png'/>
-                    </div>
+        <div className='homeFooterContainer'>
+             <footer className="homeFooter">
+                <Link>Contact</Link>
+                <Link>Facebook</Link>
+                <Link>Terms</Link>
+                <Link>Privacy</Link>
+            </footer>
+            <div className='footerLogoBox'>
+                <img className='footerLogo' src='images/Logo.png'/>
+                <a>© 2024 Chretien LLC</a>
             </div>
-            <div className="socialFooter">
-                <h4>Support</h4>
-                <div className="supportList"> 
-                    <Link>Home</Link>
-                    <Link to='/contact'>Contact Us</Link>
-                    <Link>FAQ</Link>
-                </div>
-            </div>
-        </footer>
+        </div>
+       
 
     )
 }
