c87a50a7e1b9a41825a186a48f34db8a292705a4
Jaron <jaronchretien@yahoo.com>
2024-12-03 11:52:27 -0600

    feat(redirect): Redirects user to facebook account

--- src/Components/HomeFooter.js
+++ src/Components/HomeFooter.js
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 const HomeFooter = () => {
+    const nav = useNavigate()
+    const redirectToFacebook = () => {
+       window.location.href = 'https://www.facebook.com/people/Chretien-Lawn-Care-Service-LLC/100055266442758/?mibextid=JRoKGi&rdid=R5ukdEeosytuSz6c&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1AtC5JacuA%2F%3Fmibextid%3DJRoKGi'
+    }
     return (
         <div className='homeFooterContainer'>
              <footer className="homeFooter">
-                <Link>Contact</Link>
-                <Link>Facebook</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link onClick={redirectToFacebook} >Facebook</Link>
                 <Link>Terms</Link>
                 <Link>Privacy</Link>
             </footer>
