5d4aaa44305feff82c00fe8d57db305152acd390
Jaron <jaronchretien@yahoo.com>
2024-12-04 13:23:36 -0600

    feat(users): Adds create user function to create the admins

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,26 @@
 const {dropTables, createTables} = require('./initdb')
 const client  = require('./index')
+const {createUser} = require('./user')
+
+const createInitialUsers = async() => {
+    console.log("Creating users")
+    const userJaron = await createUser(
+        {
+        username:"Jaronian", 
+        firstName: "Jaron",
+        lastName: "Chretien",
+        password: "Jaron1210",
+        emailAddress: "jaronchretien@yahoo.com",
+        phoneNumber: 3373687641
+        })
+    console.log("user creation complete")
+}
 
 const createDb = async() => {
     console.log("Creating database")
     await dropTables()
     await createTables()
+    await createInitialUsers()
 }
 client.connect()
 
