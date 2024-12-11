fa7b3dc93c5f7c8ab789d78132ed615ea20d9738
Jaron <jaronchretien@yahoo.com>
2024-12-11 10:18:00 -0600

    feat(bcrypt): Adds bcrypt hashing for passwords

--- api/user.js
+++ api/user.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const userRouter = express.Router()
-
+const {getUserByUsername} = require('../db/user')
 userRouter.post('/login', async(req, res, next) => {
     const {username, password} = req.body
-    
+    const user = getUserByUsername(username)
+    if(!user) {
+        res.send({error: true, message: "Inccorrect username or password"})
+    }
 })
 
 module.exports = userRouter
--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,24 @@
 const {dropTables, createTables} = require('./initdb')
 const client  = require('./index')
+const bcrypt = require('bcrypt')
 const {createUser} = require('./user')
 const {createInquiry} = require('./inquiry')
+
 const createInitialUsers = async() => {
+    const salts = 10
+    const hashJaronPassword = await bcrypt.hash('Jaron1210', salts)
     const userJaron = await createUser(
         {
         username:"Jaronian", 
         firstName: "Jaron",
         lastName: "Chretien",
-        password: "Jaron1210",
+        password: `${hashJaronPassword}`,
         emailAddress: "jaronchretien@yahoo.com",
         phoneNumber: 3373687641
         })
     console.log("user creation complete")
 }
+
 const createTestInquiry = async() => {
     let today = new Date()
     const inquiry1 = await createInquiry({
@@ -28,6 +33,7 @@ const createTestInquiry = async() => {
         date: today.getTime()
     })
 }
+
 const createDb = async() => {
     console.log("Creating database")
     await dropTables()
