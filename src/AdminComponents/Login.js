d278378bf4ea37af07b6efd86cd3e0f522f6d22a
Jaron <jaronchretien@yahoo.com>
2024-12-11 10:01:36 -0600

    feat(values): Sets value for user state on login admin page

--- src/AdminComponents/Login.js
+++ src/AdminComponents/Login.js
@@ -1,8 +1,11 @@
-
+import { useState } from "react"
 
 const Login = () => {
-    const handleSubmit = () => {
-        console.log("yuh")
+  const [userName, setUsername] = useState()
+  const [password, setPassword] = useState()
+  const [error, setError] = useState()
+    const handleSubmit = (e) => {
+        e.preventDefault()
     }
     return (
         <div className="login-container">
@@ -12,20 +15,26 @@ const Login = () => {
             <div className="form-group">
               <label htmlFor="username">Username</label>
               <input
+                value={userName}
+                onChange={(e) => setUsername(e.target.value)}
                 type="text"
                 id="username"
                 placeholder="Enter your username"
                 required
               />
+              <p></p>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
               <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 type="password"
                 id="password"
                 placeholder="Enter your password"
                 required
               />
+              <p></p>
             </div>
             <button type="submit" className="login-button">
               Log In
