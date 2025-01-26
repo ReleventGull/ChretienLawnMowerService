5e339e09aabb6d410a246df8d85881deb981027f
Jaron <jaronchretien@yahoo.com>
2025-01-25 18:31:40 -0600

    feat(validate): Adds the validate inquiry api call on frontend

--- src/Components/api.js
+++ src/Components/api.js
@@ -1,6 +1,29 @@
 const {REACT_APP_BASE_URL = 'http://localhost:4000/api' } = process.env
 const BASE_URL = REACT_APP_BASE_URL
 
+export const validateInquiry = async({email, phoneNumber, firstName, address, city, zipCode}) => {
+    try {
+        const response = await fetch(`${BASE_URL}/inquiry/create`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"  // Add the Content-Type header
+            },
+            body: JSON.stringify({
+                email: email,
+                firstName: firstName,
+                phoneNumber: phoneNumber,
+                address: address,
+                city: city,
+                zipCode: zipCode
+            })
+        }).then(result => result.json())
+        return response
+    }catch(error) {
+        console.error("There was an error creating the inquiry on the frontend api.", error)
+        throw error
+    }
+}
+
 export const createInquiry = async({email, phoneNumber, firstName, lastName, address, addressTwo, city, zipCode}) => {
     try {
         const response = await fetch(`${BASE_URL}/inquiry/create`, {
