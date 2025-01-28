45d5837218d2c3197d876ab1075d95bb2a13ceb9
Jaron <jaronchretien@yahoo.com>
2025-01-27 18:53:12 -0600

    feat(login): Finalizes the admin login and page redirect based on token status

--- src/AdminComponents/Admin.js
+++ src/AdminComponents/Admin.js
@@ -1,16 +1,21 @@
 import {Route, Routes} from 'react-router-dom'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { Login, Dashboard } from './adminexports'
 import { getAdmin } from './adminapi'
 //Folder responsible for validating the token to make sure it still works
 
 const Admin = () => {
     const navigate = useNavigate()
-
+    const loc = useLocation()
     const checkAdmin = async(token) => {
+        console.log(loc)
         const admin = await getAdmin({token: token})
-        console.log(admin)
+        if(admin.status == 200 && (loc.pathname=='/admin/login' || loc.pathname=='/admin' )){
+            navigate('/admin/dashboard')
+        }else if(admin.status !== 200){
+            navigate('/admin/login')
+        }
     }
     useEffect(() => {
         const token = localStorage.getItem("CLSToken")
