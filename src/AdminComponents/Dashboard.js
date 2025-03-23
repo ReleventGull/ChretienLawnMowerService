31713765387086b44ac3f76322ca62366214837c
Jaron <jaronchretien@yahoo.com>
2025-03-23 18:27:03 -0500

    feat(homeAdmin): Setups home admin page

--- src/AdminComponents/Dashboard.js
+++ src/AdminComponents/Dashboard.js
@@ -1,5 +1,5 @@
 
-import  {Navbar } from './adminexports'
+import  { Navbar, Home } from './adminexports'
 import { useEffect, useState } from 'react'
 import { Route, useLocation, useNavigate, Routes } from 'react-router-dom'
 const Dashboard = () => {
@@ -18,7 +18,7 @@ const Dashboard = () => {
             <Navbar setLocation={setLocation} location={location}/>
             <div className='dashboardComponentContainer'>
                 <Routes>
-                    <Route path='home' />
+                    <Route path='home' element={<Home />}/>
                 </Routes>
             </div>
         </div>
