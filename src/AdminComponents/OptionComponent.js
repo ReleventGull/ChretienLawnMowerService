81b9c127a686b080fc3029af278fbc46a044bafb
Jaron <jaronchretien@yahoo.com>
2025-03-25 12:04:26 -0500

    feat(all): Adds all option

--- src/AdminComponents/OptionComponent.js
+++ src/AdminComponents/OptionComponent.js
@@ -3,7 +3,8 @@
 const SelectOption = () => {
     return (
         <select className="optionDropdown">
-            <option className="option" selected>Open</option>
+            <option className="option" selected>All</option>
+            <option className="option">Open</option>
             <option className="option">Completed</option>
             <option className="option">Archived</option>
             <option className="option">Non Servicable</option>
